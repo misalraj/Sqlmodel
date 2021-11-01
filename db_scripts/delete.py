35eb5c828ae722f01ba65f465f9499ee5d6fa64d
Misal Raj <misalraj@gmail.com>
2021-11-01 17:16:59 +0530

    :arrow_up: CRUD script files added

---
+++ db_scripts/delete.py
@@ -0,0 +1,16 @@
+from create_db import Student
+from sqlmodel import Session, create_engine, select
+
+sqlite_url = "sqlite:///school.db"
+engine = create_engine(sqlite_url, echo=True)
+
+# Update the record
+
+with Session(engine) as session:
+    statement = select(Student).where(Student.first_name == "Vijay")
+    result = session.exec(statement)
+    student = result.one()
+    print("fetch result", student)
+
+    session.delete(student)  # Delete data
+    session.commit()
---
+++ db_scripts/read.py
@@ -0,0 +1,20 @@
+from create_db import Student
+from sqlmodel import Session, create_engine, select
+
+sqlite_url = "sqlite:///school.db"
+engine = create_engine(sqlite_url, echo=True)
+
+# Read database
+with Session(engine) as session:
+    statement = select(Student)
+    results = session.exec(statement)
+    for student in results:
+        print(student)
+        print("-" * 100)
+
+# # Read one row
+# with Session(engine) as session:
+#     statement = select(Student).where(Student.first_name=="Misal")
+#     results = session.exec(statement)
+#     for student in results:
+#         print(student)
